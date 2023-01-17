b3b5a6bdc09c8495b15986eedabc86b43d3f543d
WoodCollector <you@example.com>
2023-01-17 20:11:27 +0100

    made it burnable

--- contracts/VGOLD.sol
+++ contracts/VGOLD.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "./AllowedAddresses.sol";
 
-contract VGOLD is ERC20, AllowedAddresses {
+contract VGOLD is ERC20Burnable, AllowedAddresses {
     constructor() ERC20("Verdomi Gold", "VGOLD") {}
 
     function mint(address account, uint256 amount) external onlyAllowedAddresses {
